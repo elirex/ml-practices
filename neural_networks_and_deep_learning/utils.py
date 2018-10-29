c452b6fffd84fe08f6e7e0f06b8fa89be94b7d74
elirex <sheng79225@gmail.com>
2018-10-30 00:32:41 +0800

    Fix folder naming

--- neural_networks_and_deep_learning/utils.py
+++ neural_networks_and_deep_learning/utils.py
@@ -1,10 +1,6 @@
-#!/bin/env python
+import os
 import numpy as np
 import h5py
-import sklearn
-import sklearn.datasets
-import sklearn.linear_model
-import matplotlib.pyplot as plt
 
 def print_accuracy(set_type, Y, predicted_Y):
     print("{} accuracy: {}".format(set_type, 100 - np.mean(np.abs(predicted_Y -Y)) * 100))
@@ -21,12 +17,15 @@ def sigmoid(z):
     return s
 
 
-def load_catvnoncat_dataset():
-    train_dataset = h5py.File("./data/train_catvnoncat.h5", "r")
+def load_catvnoncat_dataset(path):
+    train_path = os.path.join(path, "train_catvnoncat.h5")
+    test_path = os.path.join(path, "test_catvnoncat.h5")
+
+    train_dataset = h5py.File(train_path, "r")
     train_set_x = np.array(train_dataset["train_set_x"][:])
     train_set_y = np.array(train_dataset["train_set_y"][:])
     
-    test_dataset = h5py.File("./data/test_catvnoncat.h5", "r")
+    test_dataset = h5py.File(test_path, "r")
     test_set_x = np.array(test_dataset["test_set_x"][:])
     test_set_y = np.array(test_dataset["test_set_y"][:])
 
@@ -36,13 +35,3 @@ def load_catvnoncat_dataset():
     test_set_y = test_set_y.reshape((1, test_set_y.shape[0]))
 
     return train_set_x, train_set_y, test_set_x, test_set_y, classes
-
-
-if __name__ == "__main__":
-    print("Test load_catvnoncat_dataset function")
-    train_x, train_y, test_x, test_y, classes = load_catvnoncat_dataset()
-    print("train_x.shape = {}".format(train_x.shape))
-    print("train_y.shape = {}".format(train_y.shape))
-    print("test_x.shape = {}".format(test_x.shape))
-    print("test_y.shape = {}".format(test_y.shape))
-    print("classes.shape = {}".format(classes.shape))
